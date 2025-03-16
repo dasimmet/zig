651c3acb359f746e9d7b963da3c860ba42f5ac38
Tobias Simetsreiter <dasimmet@gmail.com>
2025-03-16 14:38:26 +0100

    format embedded zon

--- lib/std/zon/parse.zig
+++ lib/std/zon/parse.zig
@@ -2057,6 +2057,7 @@ test "std.zon arrays and slices" {
         );
     }
 }
+
 test "std.zon enum as string" {
     const gpa = std.testing.allocator;
     // bare literal
@@ -2080,7 +2081,7 @@ test "std.zon enum as string" {
         const parsed = try fromSlice(struct {
             name: []const u8,
             type: []const u8,
-        }, gpa, ".{.name = .literal_0, .type=.literal_1}", null, .{
+        }, gpa, ".{ .name = .literal_0, .type = .literal_1 }", null, .{
             .enum_literals_as_strings = true,
         });
         defer free(gpa, parsed);
